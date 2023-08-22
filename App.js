64d8802da26afb6af97b77b765836a4ea58ade1b
Ruan227 <ruansilva.pascoal@gmail.com>
2023-08-22 19:36:53 -0300

    teste 1

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>bola</Text>
+      
+      <Image
+        style={styles.stretch}
+        source={require('./assets/icon.png')}
+      />
     </View>
   );
 }
@@ -13,8 +17,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6363',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  stretch: {
+    width: 400,
+    height: 400,
+    resizeMode: 'contain',
+    marginTop: 40,
+  },
 });
+
+
+
+
+
+
