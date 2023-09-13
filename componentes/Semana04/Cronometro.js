91bc299a3ac9c4a348cc8075b5385f29cffa66de
Ruan227 <ruansilva.pascoal@gmail.com>
2023-09-13 20:45:27 -0300

    cronometro

---
+++ componentes/Semana04/Cronometro.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Cronometro() {
+  return (
+    <View>
+      <Text>hj</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
